60c4abc26647c7f5a7f2a3e56601fd7e1ef14a5f
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2024-10-01 10:43:06 +0300

    Functionality for requesting a loan

--- Working-with-Arrays/script.js
+++ Working-with-Arrays/script.js
@@ -176,6 +176,19 @@ btnTransfer.addEventListener("click", function (e) {
   }
 });
 
+btnLoan.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && loggedAcc.movements.some((mov) => mov >= amount * 0.1)) {
+    loggedAcc.movements.push(amount);
+
+    updateUI(loggedAcc);
+  }
+  inputLoanAmount.value = "";
+});
+
 btnClose.addEventListener("click", function (e) {
   e.preventDefault();
   console.log("Delete an account");
