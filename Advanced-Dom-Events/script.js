d0ae10127e5ddf92beb2598348546f295d06c5d3
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2024-10-03 13:56:12 +0300

    Adding some effects

--- Advanced-Dom-Events/script.js
+++ Advanced-Dom-Events/script.js
@@ -13,6 +13,7 @@ const tabs = document.querySelectorAll(".operations__tab");
 const tabsContainer = document.querySelector(".operations__tab-container");
 const tabsContent = document.querySelectorAll(".operations__content");
 const navHeight = nav.getBoundingClientRect().height;
+const allSections = document.querySelectorAll(".section");
 
 const openModal = function (e) {
   e.preventDefault();
@@ -161,3 +162,40 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+// Reveal Sections
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach((sec) => {
+  sectionObserver.observe(sec);
+  sec.classList.add("section--hidden");
+});
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll("img[data-src]");
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener("load", function (e) {
+    entry.target.classList.remove("lazy-img");
+  });
+
+  observer.unobserve(entry.target);
+};
+const imageObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
+imgTargets.forEach((img) => imageObserver.observe(img));
