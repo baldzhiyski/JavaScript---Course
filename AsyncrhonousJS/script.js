68bdc030fe0f1ad36b3e7e53d248ff8a879f0603
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2024-10-08 12:11:30 +0300

    Nested callbacks ( callback hell )

--- AsyncrhonousJS/script.js
+++ AsyncrhonousJS/script.js
@@ -35,3 +35,56 @@ const countries = ["portugal", "bulgaria", "france", "germany", "usa"];
 // };
 
 // countries.forEach((country) => getCountryData(country));
+
+const renderCountry = function (data, className) {
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>
+    `;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  const request = new XMLHttpRequest();
+  request.open(
+    "GET",
+    `https://countries-api-836d.onrender.com/countries/name/${country}`
+  );
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    renderCountry(data);
+
+    // Get Neighbour Country
+    const neighbour = data.borders?.[0];
+
+    if (neighbour === undefined) return;
+
+    const request2 = new XMLHttpRequest();
+    request2.open(
+      "GET",
+      `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+    );
+    request2.send();
+
+    request2.addEventListener("load", function () {
+      const data2 = JSON.parse(this.responseText);
+
+      renderCountry(data2, "neighbour");
+    });
+  });
+};
+
+getCountryAndNeighbour("usa");
